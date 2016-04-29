b533be498247ee0951f4eb74a06daf1820ad80f1
Johnny Hu <jgeehen.hu@gmail.com>
2016-04-28 23:49:34 -0700

    setup css/scss folder structure; rename static_pages index.html to home.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
+  resources :posts
+  root 'static_pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
